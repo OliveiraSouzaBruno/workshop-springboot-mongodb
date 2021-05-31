87cf07c67a6d1d6fd65debc720af638f9a55ed7e
Bruno de Souza Oliveira <oliveira_bruno2018@hotmail.com>
2021-05-31 16:56:48 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/bruno/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.bruno.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bruno.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
